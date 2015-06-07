e550a1f28ff271e963f77cd71bbf7c41e3312966
DiegoE3 <kvasir3cursos@gmail.com>
2015-06-07 23:22:32 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,17 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página de entrada (homepage)
 router.get('/', function(req, res) {
   res.render('index', { title: 'EncueZta' });
 });
 
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load); //autoload :quizId
+
 //Definición de rutas de /quizes
-router.get('/quizes/',                 quizController.index);
-router.get('/quizes/:quizId(\\d+)',    quizController.show);
+router.get('/quizes/',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',         quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
 
 /*P2P obligatorio modulo 6, apartado 2b*/
